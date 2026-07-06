796f5564e2039cd791601f632531b78f86256381
kittythecat019 <kittythecat019@gmail.com>
2026-07-06 15:31:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -45,10 +45,12 @@ app.post(
 
 );
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
 
     console.log(
-        "Server running on port 3000"
+        `Server running on port ${PORT}`
     );
 
 });
